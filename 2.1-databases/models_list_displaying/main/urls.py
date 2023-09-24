bf06bb9e1cbe6542959fac228036636e902f7ea8
lyapdy <lyapdy@gmail.com>
2023-09-24 21:14:45 +0700

    books_list

--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, book_date
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('books/', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/<pub_date>/', book_date, name='book_date'),
 ]
