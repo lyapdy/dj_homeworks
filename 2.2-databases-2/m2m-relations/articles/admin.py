de3dfe5e112dba7b0a8a683637baf416c538c8fe
lyapdy <lyapdy@gmail.com>
2023-10-11 17:32:19 +0700

    m2m-relations-lyapunov

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -3,6 +3,52 @@ from django.contrib import admin
 from .models import Article
 
 
+####################################
+from django.contrib import admin
+
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, ArticleTags, Tag
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+class ArticleTagsInlineFormset(BaseInlineFormSet):
+    def clean(self):
+
+        if len(self.forms) == 0:
+            raise ValidationError('Не указаны теги!')
+
+        self.count_is_main_tag = 0
+
+        for form in self.forms:
+            if self.count_is_main_tag > 0 and form.cleaned_data.get('is_main'):
+                raise ValidationError('Главный может быть только 1 тег')
+            else:
+                if form.cleaned_data.get('is_main'):
+                    print(f"{form.cleaned_data.get('tag')} - главный раздел")
+                    self.count_is_main_tag += 1
+                else:
+                    continue
+
+        return super().clean()
+
+
+class ArticleTagsInline(admin.TabularInline):
+    model = ArticleTags
+    formset = ArticleTagsInlineFormset
+    extra = 3
+
+
+#####################################
+
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title']
+    inlines = [ArticleTagsInline]
+    #path
+
+
+
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
-from articles.models import Article
+from articles.models import Article, Tag
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    ordering = '-published_at'
+    context = {
+        'object_list': Article.objects.order_by(ordering)
+    }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    #ordering = '-published_at'
 
     return render(request, template, context)
