b14c5e1ff18cb1d17898edd1a1ba282a8a383414
lyapdy <lyapdy@gmail.com>
2023-10-11 20:00:32 +0700

    orm-migrations_lyapunov

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -3,13 +3,31 @@ from django.shortcuts import render
 
 from .models import Student
 
-
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
+    obj_list = Student.objects.all()
+    context = {
+        'object_list': obj_list
+    }
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
 
     return render(request, template, context)
+
+
+# def students_list(request):
+#     template = 'school/students_list.html'
+#     context = {}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    #ordering = 'group'
+
+    ###############################################
+
+
+
+    # ##############################################
+    #
+    # return render(request, template, context)
