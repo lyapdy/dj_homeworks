12267162e786adab7b6a6ff600b78dd266be830f
lyapdy <lyapdy@gmail.com>
2023-10-26 08:04:34 +0700

    django-testing

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -80,7 +80,11 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_django_testing',
+        'USER': 'postgres',
+        'PASSWORD': 'qazxswedc123',
+        'NAME': 'netology_stocks_products',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -124,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MAX_STUDENTS_PER_COURSE = 20
--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
+from django_testing import settings
 from students.models import Course
 
 
@@ -8,3 +9,10 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate_students(self, value):
+        if len(value) > settings.MAX_STUDENTS_PER_COURSE:
+            raise exceptions.ValidationError(
+                f'Превышено максимальное число студентов на курсе: {settings.MAX_STUDENTS_PER_COURSE}'
+            )
+        return value
